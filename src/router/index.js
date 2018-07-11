c92005a9fea5cad24588169ca17156b65f5b98c0
Masahiko Sato <sato_m@mediaxis.jp>
2018-07-11 23:29:37 +0900

    redirect to /signin

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,10 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '*',
+      redirect: 'signin'
+    },
+    {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
