7f380ad79a15d6da66500cd8d43692a41a722bd4
Masahiko Sato <sato_m@mediaxis.jp>
2018-07-12 22:20:26 +0900

    setup firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+}
+
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
